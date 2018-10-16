e78375448ea142f89b9f054b4b299dea9626437c
aaa <aaa@itcast.com>
2018-10-16 11:30:17 +0800

    提交git_test

---
+++ git_test/src/com/itheima/Demo1.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
